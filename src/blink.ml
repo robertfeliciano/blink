74f8922ae4c156db6f987ae574b972dcce2c91e3
robertfeliciano <robertfeliciano314@gmail.com>
2025-01-25 19:31:11 -0500

    restructure

--- src/blink.ml
+++ src/blink.ml
@@ -1,52 +1,33 @@
-open Arg
+open Core
+open Compiler
 
-module Range = Util.Range
+let get_file_ext filename =
+  String.split_on_chars filename ~on:['.'] |> List.last |> Option.value ~default:""
 
-let read_file (file:string) : string =
-  let channel = open_in file in
-  let lines = ref [] in
-  try while true; do
-      lines := input_line channel :: !lines
-  done; ""
-  with End_of_file ->
-    close_in channel;
-    String.concat "\n" (List.rev !lines)
+let blink_file =
+  let error_not_file filename =
+    eprintf "'%s' is not a blink file" filename ;
+    exit 1 in
+  Command.Spec.Arg_type.create (fun filename ->
+      match Sys_unix.file_exists filename with
+      | `Yes           ->
+          if get_file_ext filename |> String.equal "bl" then filename
+          else error_not_file filename
+      | `No | `Unknown -> error_not_file filename)
 
-let parse filename =
-  let lexbuf = read_file filename |> Lexing.from_string in
-  try
-    Parser.prog Lexer.read lexbuf
-  with
-  | Parser.Error -> 
-    failwith @@ Printf.sprintf "Parse error at: %s"
-      (Range.string_of_range (Range.lex_range lexbuf))
+let command = 
+  Command.basic ~summary:"Don't Blink."
+  ~readme:(fun () -> "List of Blink options.")
+  Command.Let_syntax.(
+  let%map_open _print_ast = 
+    flag "-print-init-ast" no_arg ~doc:"Pretty print the initial AST"
+  and _print_tast = 
+    flag "-print-typed-ast" no_arg ~doc:"Pretty print the typed AST"
+  and filename = anon (maybe_with_default "-" ("filename" %: blink_file)) in 
+  fun () -> 
+    In_channel.with_file filename ~f:(fun ic -> 
+      Lexing.from_channel ic |> compile ~_print_ast ~_print_tast
+      ))
 
-let print_ast_flag = ref false
 
-let args = 
-  [ ("--print-ast", Set print_ast_flag, "prints the ast of the program")]
-
-let files = ref []
-
-let () = 
-  try 
-    (* let args = Array.to_list Sys.argv in 
-    let filename = 
-    match args with
-      | [] | [_] -> (* This case is impossible because Sys.argv always has at least one element *)
-        failwith "No arguments provided."
-      | _ :: file :: [] -> 
-        file
-      | _ -> 
-        failwith "Multiple files not supported yet!"
-    in  *)
-    Arg.parse args (fun filename -> files := filename :: !files) "Blink compiler";
-    let ast = Desugar.desugar  @@ parse @@ List.hd !files in 
-    begin
-      Printf.printf("Successful parse\n");
-      if !print_ast_flag then 
-        Astlib.print_ast ast
-    end
-  with 
-  | Failure msg -> Printf.eprintf "Error: %s\n" msg
-  | e -> Printf.eprintf "Unexpected error: %s\n" (Printexc.to_string e)
+  let () = Command_unix.run ~version:"1.0" ~build_info:"RWO" command
---
+++ src/compiler.ml
@@ -0,0 +1,4 @@
+open Core
+
+let compile ?(_print_ast=false) ?(_print_tast=false) _file =
+  printf "hi\n"
