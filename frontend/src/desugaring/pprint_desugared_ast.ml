187257cae2cae46c177ba0e052cd8db99dc7b446
robertfeliciano <robertfeliciano314@gmail.com>
2025-10-26 15:59:37 -0400

    remove redundant printers

--- frontend/src/desugaring/pprint_desugared_ast.ml
+++ frontend/src/desugaring/pprint_desugared_ast.ml
@@ -1,14 +1,11 @@
 open Desugared_ast
 module PT = Typing.Pprint_typed_ast
-module Base_PT = Typing.Typed_ast
 
 (* Since most types in desugared_ast alias Typed_ast,
    we can reuse the Pprint_typed_ast show functions directly. *)
 
 let show_ty = PT.show_ty
 let show_ret_ty = PT.show_ret_ty
-let show_unop = Base_PT.show_unop
-let show_binop = Base_PT.show_binop
 let show_exp = PT.show_exp
 let show_vdecl = PT.show_vdecl
 let show_stmt = PT.show_stmt
