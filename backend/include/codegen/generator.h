7898356fa756d2d2f253ab553f6735f421b03596
robertfeliciano <robertfeliciano314@gmail.com>
2025-11-13 20:48:34 -0500

    llvm types

---
+++ backend/include/codegen/generator.h
@@ -0,0 +1,71 @@
+#pragma once
+
+#include <stdlib.h>
+
+#include <string>
+#include <map>
+
+#include "llvm/IR/IRBuilder.h"
+#include "llvm/IR/LLVMContext.h"
+#include "llvm/IR/Module.h"
+#include "llvm/IR/Type.h"
+#include "llvm/IR/Value.h"
+#include "llvm/Support/TargetSelect.h"
+#include "llvm/Support/Host.h"
+#include "llvm/Target/TargetMachine.h"
+#include "llvm/Transforms/InstCombine/InstCombine.h"
+#include "llvm/Transforms/Utils/Mem2Reg.h"
+#include "llvm/IR/PassManager.h"
+#include "llvm/Passes/PassBuilder.h"
+#include "llvm/Transforms/Scalar/GVN.h"
+#include "llvm/Transforms/Scalar/SimplifyCFG.h"
+#include "llvm/Transforms/Scalar/Reassociate.h"
+#include "llvm/Analysis/AliasAnalysis.h"
+#include "llvm/Analysis/TargetLibraryInfo.h"
+
+#include <bridge/exp.h>
+#include <bridge/stmt.h>
+#include <bridge/prog.h>
+#include <codegen/types.h>
+
+struct Generator {
+    std::unique_ptr<llvm::LLVMContext> ctxt;
+    std::unique_ptr<llvm::IRBuilder<>> builder;
+    std::unique_ptr<llvm::Module> mod;
+
+    std::map<std::string, llvm::AllocaInst *> varEnv;
+    
+    void configureTarget();
+
+    void optimize();
+
+    Generator();
+
+    // llvm::Value* codegenExp(const Exp& e) {
+    //     return std::visit(ExpToLLVisitor(*this), e.val);
+    // }
+
+    // llvm::Value* codegenStmt(const Stmt& s) {
+    //     return std::visit(StmtToLLVisitor(*this), s.val);
+    // }
+
+    llvm::Type* codegenType(const Ty& ty) {
+        return TypeToLLGenerator(*this).codegenTy(ty);
+    }
+
+    llvm::Type* codegenRetType(const RetTy& ty) {
+        return TypeToLLGenerator(*this).codegenRetTy(ty);
+    }
+
+    // void codegenFunctionProtos(const Program& p) {
+    //     return DeclToLLVisitor(*this).codegenFunctionProtos(p);
+    // }
+
+    // void codegenDecl(const Decl& d) {
+    //     return std::visit(DeclToLLVisitor(*this), d.val);
+    // }
+
+    void codegenProgram(const Program& p);
+
+    void dumpLL(const std::string& filename);
+};
---
+++ backend/src/codegen/types.cpp
@@ -0,0 +1,65 @@
+#include <codegen/generator.h>
+#include <codegen/types.h>
+
+llvm::Type* TypeToLLGenerator::createSignedInt(const Sint si) {
+    switch (si) {
+        case Sint::Ti8:
+            return llvm::Type::getInt8Ty(*gen.ctxt);
+        case Sint::Ti16:
+            return llvm::Type::getInt16Ty(*gen.ctxt);
+        case Sint::Ti32:
+            return llvm::Type::getInt32Ty(*gen.ctxt);
+        case Sint::Ti64:
+            return llvm::Type::getInt64Ty(*gen.ctxt);
+        case Sint::Ti128:
+            return llvm::Type::getInt128Ty(*gen.ctxt);   
+    }
+}
+
+llvm::Type* TypeToLLGenerator::createUnsignedInt(const Uint ui) {
+    switch (ui) {
+        case Uint::Tu8:
+            return llvm::Type::getInt8Ty(*gen.ctxt);
+        case Uint::Tu16:
+            return llvm::Type::getInt16Ty(*gen.ctxt);
+        case Uint::Tu32:
+            return llvm::Type::getInt32Ty(*gen.ctxt);
+        case Uint::Tu64:
+            return llvm::Type::getInt64Ty(*gen.ctxt);
+        case Uint::Tu128:
+            return llvm::Type::getInt128Ty(*gen.ctxt);   
+    }
+}
+
+llvm::Type* TypeToLLGenerator::codegenTy(const Ty& ty) {
+    switch (ty.tag) {
+        case TyTag::TBool:
+            return llvm::Type::getInt1Ty(*gen.ctxt);
+        case TyTag::TInt: {
+            switch (ty.int_ty->tag) {
+                case IntTyTag::Signed:
+                    return createSignedInt(ty.int_ty->sint);
+                case IntTyTag::Unsigned:
+                    return createUnsignedInt(ty.int_ty->uint);
+            }
+        }
+        case TyTag::TFloat: 
+            switch(*ty.float_ty) {
+                case FloatTy::Tf32: 
+                    return llvm::Type::getFloatTy(*gen.ctxt);
+                case FloatTy::Tf64:
+                    return llvm::Type::getDoubleTy(*gen.ctxt);
+            }
+        default: 
+            throw std::runtime_error("Other type not supported yet");
+    }
+}
+
+llvm::Type* TypeToLLGenerator::codegenRetTy(const RetTy& rty) {
+    switch (rty.tag) {
+        case RetTyTag::RetVoid:
+            return llvm::Type::getVoidTy(*gen.ctxt);
+        case RetTyTag::RetVal:
+            return codegenTy(*rty.val);
+    }
+}
